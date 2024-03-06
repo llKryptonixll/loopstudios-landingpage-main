c07492a9eab08fc9612c6e32d8f6fb2e1dbc7cfb
Lucas Cerri <lucascerri3@outlook.de>
2024-03-06 16:50:40 +0100

    fixed wrong hero background path

--- src/components/Hero_Section.jsx
+++ src/components/Hero_Section.jsx
@@ -2,7 +2,7 @@ import Navbar from "../layout/Navbar"
 
 const Hero_Section = () => {
     return (
-        <header className="relative grid items-start sm:bg-[url('images/desktop/image-hero.jpg')] bg-[url('images/mobile/image-hero.jpg')] min-h-[650px] lg:px-main_page_padding_large px-main_page_padding_small pt-14 bg-cover bg-no-repeat">
+        <header className="relative grid items-start sm:bg-[url('/images/desktop/image-hero.jpg')] bg-[url('/images/mobile/image-hero.jpg')] min-h-[650px] lg:px-main_page_padding_large px-main_page_padding_small pt-14 bg-cover bg-no-repeat">
             <div className="bg-black absolute top-0 left-0 w-full h-full opacity-40"></div>
             <Navbar />
             <div>
